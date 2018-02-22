675604118a33b898aaec78c1f1e8f4d055673aee
Nikhil Mishra <12347532+Curious-Nikhil@users.noreply.github.com>
2018-02-23 00:27:41 +0530

    Added mars with poor orbit path
    Bard mars orbit. 
    another issue

--- SSS_V5.0_Hohmanns Transfer/sketch.js
+++ SSS_V5.0_Hohmanns Transfer/sketch.js
@@ -32,7 +32,7 @@ function setup() {
     stars[i] = new Star();
   }
   earth = new Planet(20 /*size*/, 1/*a*/, 0.0167/*e*/, 0.985608/*n*/, 357.529/*Mo*/, 100/*nowday*/, 174.873/*cO*/, 288.064/*O*/, 0/*i*/);
-  mars = new Planet(10, marsdata.a, marsdata.e, marsdata.lomega)
+  mars = new Planet(10, 1.524, 0.0935, 0.524039, 19.373, 100, 49.558, 286.502, 1.850)
 }
 
 function draw() {
@@ -42,6 +42,8 @@ function draw() {
   earth.ephemeris();
   earth.show();
   earth.gui();
+  mars.ephemeris();
+  mars.show();
   text(2000 + earth.nd/365, 10, 20);
 
   for ( var i = 0; i < stars.length; i++) {
